93a9ffda74dfce37bf2f61ccd01b9fd04cdd5172
missymagenta <alina.soare37@gmail.com>
2019-08-13 18:02:25 +0200

    third commit

--- authenticate/views.py
+++ authenticate/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
-
+from django.contrib.auth.forms import UserCreationForm
+from .forms import SignUpForm
 
 def home(request):
     return render(request, 'authenticate/home.html', {})
@@ -31,4 +32,18 @@ def logout_user(request):
 
 
 def register_user(request):
-    return render(request, 'authenticate/register.html', {})
+    if request.method == "POST":
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user = authenticate(request, username=username, password=password)
+            login(request, user)
+            messages.success(request, ('Registration succesfull!!'))
+            return redirect('home')
+    else:
+        form = SignUpForm()
+
+    context = {'form': form}
+    return render(request, 'authenticate/register.html', context)
